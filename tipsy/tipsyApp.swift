cc18e5798beb5e2c78b357bc26ba99e695f8dadd
gatts123 <agattani@seas.upenn.edu>
2024-06-22 17:21:14 -0700

    Initial Commit

---
+++ tipsy/tipsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipsyApp.swift
+//  tipsy
+//
+//  Created by Arnav Gattani on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct tipsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
